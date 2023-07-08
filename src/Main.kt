4e3f198728c0221d05451a043a65153a65554e48
bleeeana <bobkov.v03@icloud.com>
2023-07-09 00:53:52 +0300

    pre-second version

--- src/Main.kt
+++ src/Main.kt
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package org.jetbrains.kotlin.Math
 import javafx.stage.Stage
 import java.io.File
@@ -9,16 +8,4 @@ val scan = java.util.Scanner(System.`in`)
 fun main(){
     val Visual = Vizualisation()
     Application.launch(Vizualisation::class.java)
-=======
-package org.jetbrains.kotlin.Math
-import javafx.stage.Stage
-import java.io.File
-
-import javafx.application.Application
-
-val scan = java.util.Scanner(System.`in`)
-fun main(){
-    val Visual = Vizualisation()
-    Application.launch(Vizualisation::class.java)
->>>>>>> 5fa09004ea99ccf1b3b5822615b52576546331cc
 }
--- src/VertexVizualisation.kt
+++ src/VertexVizualisation.kt
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package org.jetbrains.kotlin.Math
 
 import javafx.scene.Group
@@ -26,35 +25,4 @@ class VertexVizualisation(scene_size : Double, x : Double, y : Double, name_vert
         data = Group(circle,text)
 
     }
-
-=======
-package org.jetbrains.kotlin.Math
-
-import javafx.scene.Group
-import javafx.scene.paint.Color
-import javafx.scene.shape.Circle
-import javafx.scene.shape.Shape
-import javafx.scene.text.Font
-import javafx.scene.text.Text
-// класс для визуализации вершины, состоящей из круга и имени(символ латинского алфавита)pr
-class VertexVizualisation(scene_size : Double, x : Double, y : Double, name_vertex: String, val number: Int ) : Shape(){
-    var name: Text
-    var circle: Circle
-    var data : Group
-    init {
-        circle = Circle(x,y, scene_size / 25.0)
-        circle.fill = Color.PINK
-        val text = Text(name_vertex)
-        text.font = Font.font("Arial",15.0)
-        this.name = text
-        circle.strokeWidth = 3.0
-        circle.stroke = Color.BLACK
-        text.fill = Color.BLACK
-        text.x = circle.centerX - text.layoutBounds.width / 2.0
-        text.y = circle.centerY + text.layoutBounds.height / 4.0
-        data = Group(circle,text)
-
-    }
-
->>>>>>> 5fa09004ea99ccf1b3b5822615b52576546331cc
 }
