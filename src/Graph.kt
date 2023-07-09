8fe7febe25f2ebe4be2fb9488ea3c364043d4f6a
Dariiiii <usadariaa@yandex.ru>
2023-07-10 01:55:11 +0300

    second_version

--- src/Graph.kt
+++ src/Graph.kt
@@ -3,52 +3,45 @@ package org.jetbrains.kotlin.Math
 import java.io.File
 import java.util.*
 
-// класс для графа. Граф представлен матрицей смежности, если ребра между вершинами нет, то в соответстсвующей ячейке будет значение Int.MAXVALUE
 open class Graph(var start_vertex: Int = 0) {
-    //mutableListOf - представляет собой динамически расширяемый список элементов.
     var name_vertex: MutableList<String> = mutableListOf()
     var data = mutableListOf<MutableList<Int>>()
-//    var start_vertex: Int = 0
 
-    // метод для чтения матрицы из файла
+    fun default_name(quotient: Int): String {
+        val base = 26
+        val sb = StringBuilder()
+        var coefficient = quotient
+        while (coefficient > 0) {
+            coefficient--
+            val remainder = coefficient % base
+            sb.append(('A'.code + remainder).toChar())
+            coefficient /= base
+        }
+        return (sb.reverse().toString())
+    }
+
     fun read_from_file(filename: String) {
         try {
             val file = File(filename)
             val scanner = Scanner(file)
             println("Если вы желаете задать имена вершин в файле, введите \"1\". Введите \"2\", чтобы имена вершин были заданы автоматически.")
             val flag = readln().toInt()
-            if (flag == 1) {
-                this.name_vertex = scanner.nextLine().split(" ").toMutableList()
-            }
+            if (flag == 1) this.name_vertex = scanner.nextLine().split(" ").toMutableList()
             while (scanner.hasNextLine()) {
-                var line = scanner.nextLine()
-                var row = line.split(" ").map { it.toInt() }.toMutableList()
+                val line = scanner.nextLine()
+                val row = line.split(" ").map { it.toInt() }.toMutableList()
                 data.add(row)
             }
             if (flag == 2) {
-                val base = 26
-                var quotient: Int
                 for (i in 0 until data.size) {
-                    quotient = i + 1
-                    val sb = StringBuilder()
-                    while (quotient > 0) {
-                        quotient--
-                        val remainder = quotient % base
-                        sb.append(('A'.code + remainder).toChar())
-                        quotient /= base
-                    }
-                    this.name_vertex.add(sb.reverse().toString())
+                    this.name_vertex.add(default_name(i + 1))
                 }
             }
             modificate()
-
-        } catch (e: Exception) {
-            println("Файл не найден: $filename")
-
         }
+        catch (e: Exception) { println("Файл не найден: $filename") }
     }
 
-    // метод для чтения матрицы из консоли
     fun read_from_console() {
         val scan = Scanner(System.`in`)
         println("Сколько вершин должно быть в графе?")
@@ -62,18 +55,8 @@ open class Graph(var start_vertex: Int = 0) {
             }
 
             2 -> {
-                val base = 26
-                var quotient: Int
                 for (i in 0 until size) {
-                    quotient = i + 1
-                    val sb = StringBuilder()
-                    while (quotient > 0) {
-                        quotient--
-                        val remainder = quotient % base
-                        sb.append(('A'.code + remainder).toChar())
-                        quotient /= base
-                    }
-                    this.name_vertex.add(sb.reverse().toString())
+                    this.name_vertex.add(default_name(i + 1))
                 }
             }
         }
@@ -86,8 +69,7 @@ open class Graph(var start_vertex: Int = 0) {
         modificate()
     }
 
-    // метод, который возвращает строку для вывода информации о графе в консоль
-    //????????????
+
     override fun toString(): String {
         var string: String = ""
         data.forEach() {
@@ -96,22 +78,16 @@ open class Graph(var start_vertex: Int = 0) {
         return string
     }
 
-    // метод для переопределения весов ребер(если в файле было -1, значит ребра нет)
     fun modificate() {
         for (i in 0 until data.size) {
             for (j in 0 until data[i].size) {
-                if (data[i][j] == -1) {
-                    data[i][j] = Int.MAX_VALUE
-                }
+                if (data[i][j] == -1) data[i][j] = Int.MAX_VALUE
             }
         }
     }
 
-    fun set_start_vertex(new_start_vertex: Int) {
-        this.start_vertex = new_start_vertex
-    }
+    fun set_start_vertex(new_start_vertex: Int) { this.start_vertex = new_start_vertex }
 
-    // реализация алгоритма Прима для графа, возвращает массив пар, соответствующих началу и концу ребра
     fun PrimAlgorithm(): Pair<Pair<MutableList<Pair<Int, Int>>, MutableList<MutableList<Pair<Int, Int>>>>, MutableList<Int>> {
         val edges_considered_at_the_step: MutableList<MutableList<Pair<Int, Int>>> = mutableListOf()
         val result_edges: MutableList<Pair<Int, Int>> = mutableListOf()
@@ -128,19 +104,28 @@ open class Graph(var start_vertex: Int = 0) {
         return Pair(Pair(result_edges, edges_considered_at_the_step), added_vertexes)
     }
 
+    fun correct_graph(): Boolean {
+        for (i in 0 until data.size) {
+            var flag = false
+            for (j in 0 until data[i].size) {
+                if (data[i][j] != Int.MAX_VALUE) flag = true
+            }
+            if (!flag) {
+                println("Созданный граф является несвязным или задано не более двух вершин.")
+                return false
+            }
+        }
+        return true
+    }
+
     fun iterated_PrimAlgorithm(result_edges: MutableList<Pair<Int, Int>>, added_vertexes: MutableList<Int>,
-        edges_considered_at_the_step: MutableList<Pair<Int, Int>>, ) {
+                               edges_considered_at_the_step: MutableList<Pair<Int, Int>> ) {
         var min = Int.MAX_VALUE
         var new_edge = 0 to 0
         for (i in 0 until added_vertexes.size) {
-            for (j in i until data[added_vertexes[i]].size) {
+            for (j in 0 until data[added_vertexes[i]].size) {
                 if (j !in added_vertexes) {
-                    if (data[added_vertexes[i]][j] < Int.MAX_VALUE) edges_considered_at_the_step.add(
-                        Pair(
-                            added_vertexes[i],
-                            j
-                        )
-                    )
+                    if (data[added_vertexes[i]][j] < Int.MAX_VALUE) edges_considered_at_the_step.add(Pair(added_vertexes[i], j))
                     if (data[added_vertexes[i]][j] < min) {
                         min = data[added_vertexes[i]][j]
                         new_edge = Pair(added_vertexes[i], j)
@@ -150,5 +135,18 @@ open class Graph(var start_vertex: Int = 0) {
         }
         added_vertexes.add(new_edge.second)
         result_edges.add(new_edge)
+    }
+
+    fun create_new_vertex(name : String) {
+        for (i in 0 until data.size) data[i].add(Int.MAX_VALUE)
+        data.add(MutableList(data.size + 1) { Int.MAX_VALUE })
+        name_vertex.add(name)
+    }
+
+    fun delete_vertex(number : Int){
+        data.removeAt(number - 1)
+        for (string in data){
+            string.removeAt(number - 1)
+        }
     }
 }
--- src/GraphVizualisation.kt
+++ src/GraphVizualisation.kt
@@ -4,29 +4,27 @@ import javafx.scene.Group
 import kotlin.math.cos
 import kotlin.math.sin
 
-//класс для визуализации графа(множество ребер и множество вершин)
 class GraphVizualisation(scene_size: Double, val graph: Graph) {
     var vertexes = mutableListOf<VertexVizualisation>()
     val radius = (3.0 / 8.0) * scene_size
     var edges = mutableListOf<MutableList<Edge>>()
     var full_graph = Group()
     var step = -1
-
     init {
-        var previous_y = (3.0 / 4.0) * scene_size
-        var previous_x = scene_size / 2.0
+        var previous_y : Double
+        var previous_x : Double
         val alpha0 = 2.0 * Math.PI / graph.data.size.toDouble()
         for (i in 0 until graph.data.size) {
             val alpha = alpha0 * i
-            previous_x += radius * cos(alpha)
-            previous_y -= radius * sin(alpha)
+            previous_x = radius * cos(alpha) + 500.0
+            previous_y = radius * sin(alpha) + scene_size / 2.0
             vertexes.add(VertexVizualisation(scene_size, previous_x, previous_y, graph.name_vertex[i], i + 1))
         }
         for (i in 0 until graph.data.size) {
             edges.add(mutableListOf<Edge>())
-            for (j in 0 until i) {
+            for (j in 0 until i + 1) {
                 if (graph.data[i][j] != Int.MAX_VALUE) {
-                    edges[i].add(Edge(vertexes[i], vertexes[j], graph.data[i][j]))
+                    edges[i].add(Edge(vertexes[j], vertexes[i], graph.data[i][j]))
                 }
             }
         }
@@ -55,5 +53,57 @@ class GraphVizualisation(scene_size: Double, val graph: Graph) {
         if (step > -1) step -= 1
         else step = -1
         return step
+    }
+
+    fun add_edge(new_edge : Edge) {
+        if (new_edge.position_2 != new_edge.position_1) {
+            graph.data[new_edge.position_1][new_edge.position_2] = new_edge.weight
+            graph.data[new_edge.position_2][new_edge.position_1] = new_edge.weight
+            for (edge in edges[new_edge.position_2]) {
+                if (edge.position_1 == new_edge.position_1) {
+                    edges[new_edge.position_2].remove(edge)
+                    full_graph.children.remove(edge.edgegroup)
+                    break
+                }
+            }
+            edges[new_edge.position_2].add(new_edge)
+            full_graph.children.add(0,new_edge.edgegroup)
+        }
+    }
+
+    fun add_vertex(new_vertex: VertexVizualisation){
+        graph.create_new_vertex(new_vertex.name.text)
+        vertexes.add(new_vertex)
+        edges.add(mutableListOf<Edge>())
+        full_graph.children.add(new_vertex.data)
+    }
+
+    fun delete_vertex(vertex_to_delete : VertexVizualisation) {
+        graph.delete_vertex(vertex_to_delete.number)
+        full_graph.children.remove(vertex_to_delete.data)
+        for (edge_list in edges) {
+            for (edge in edge_list) {
+                if (edge.position_2 == vertex_to_delete.number - 1 || edge.position_1 == vertex_to_delete.number - 1) {
+                    full_graph.children.remove(edge.edgegroup)
+                }
+            }
+        }
+        vertexes.remove(vertex_to_delete)
+        for (i in vertex_to_delete.number - 1 until vertexes.size) {
+            vertexes[i].number -= 1
+        }
+        for (edgeline in edges){
+            for(edge in edgeline){
+                if (edge.position_1 > vertex_to_delete.number - 1) edge.position_1 --
+                if (edge.position_2 > vertex_to_delete.number - 1) edge.position_2 --
+            }
+        }
+    }
+
+    fun delete_edge(edge : Edge){
+        graph.data[edge.position_1][edge.position_2] = Int.MAX_VALUE
+        graph.data[edge.position_2][edge.position_1] = Int.MAX_VALUE
+        edges[edge.position_2].remove(edge)
+        full_graph.children.remove(edge.edgegroup)
     }
 }
